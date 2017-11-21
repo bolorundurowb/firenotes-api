139bd091ac721b4cf37e0be7f82776f2e2c2d7cb
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-21 07:15:30 +0100

    feature: add default route responses

---
+++ firenotes-api/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firenotes_api.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private string response = "Welcome to the Firenotes API.";
+        
+        // GET
+        [HttpGet]
+        [Route("/")]
+        public string Root()
+        {
+            return $"{response} Start by making requests to the /api routes";
+        }
+        
+        // GET
+        [HttpGet]
+        [Route("/api")]
+        public string ApiRoot()
+        {
+            return response;
+        }
+    }
+}
