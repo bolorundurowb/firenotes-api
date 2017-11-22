a372ff9ece457289cc5bf72f0ac0a11d74bb22c5
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-22 23:31:26 +0100

    add default constrcutor implementation

--- firenotes-api/Models/User.cs
+++ firenotes-api/Models/User.cs
@@ -2,15 +2,21 @@
  * Created by winner-timothybolorunduro on 22/11/2017.
  */
 
+using MongoDB.Bson.Serialization.Attributes;
 using shortid;
 
 namespace firenotes_api.Models
  {
      public class User
      {
+         [BsonId]
          public string Id { get; set; }
          
-         public string Username { get; set; }
+         public string FirstName { get; set; }
+
+         public string LastName { get; set; }
+         
+         public string Email { get; set; }
          
          public string HashedPassword { get; private set; }
 
@@ -21,6 +27,11 @@ namespace firenotes_api.Models
                  var salt = BCrypt.Net.BCrypt.GenerateSalt();
                  HashedPassword = BCrypt.Net.BCrypt.HashPassword(value, salt);
              }
+         }
+
+         public User()
+         {
+             Id = ShortId.Generate(false, true, 10);
          }
      }
  }
