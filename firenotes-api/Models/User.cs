b10426e04045877764ef82cc14e23cfee3a53774
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-22 23:22:38 +0100

    add user model properties and the bcrypt lib

--- firenotes-api/Models/User.cs
+++ firenotes-api/Models/User.cs
@@ -1,11 +1,25 @@
 /**
  * Created by winner-timothybolorunduro on 22/11/2017.
  */
- 
- namespace firenotes_api.Models
+
+using shortid;
+
+namespace firenotes_api.Models
  {
      public class User
      {
+         public string Id { get; set; }
          
+         public string Username { get; set; }
+         
+         public string HashedPassword { get; }
+
+         public string Password
+         {
+             set
+             {
+                 
+             }
+         }
      }
  }
