8f751062ec37c98c8e5eaae87486848c5eccfaea
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-23 12:26:24 +0100

    feature: add model tests

---
+++ firenotes-api.Tests/Unit/Models/Note.Tests.cs
@@ -0,0 +1,16 @@
+﻿using firenotes_api.Models.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace firenotes_api.Tests.Models
+{
+    public class Note_Tests
+    {
+        [Fact]
+        public void ShouldAutoGenerateId()
+        {
+            var note = new Note();
+            note.Id.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
---
+++ firenotes-api.Tests/Unit/Models/User.Tests.cs
@@ -0,0 +1,16 @@
+﻿using firenotes_api.Models.Data;
+using FluentAssertions;
+using Xunit;
+
+namespace firenotes_api.Tests.Models
+{
+    public class User_Tests
+    {
+        [Fact]
+        public void ShouldAutoGenerateId()
+        {
+            var user = new User();
+            user.Id.Should().NotBeNullOrWhiteSpace();
+        }
+    }
+}
