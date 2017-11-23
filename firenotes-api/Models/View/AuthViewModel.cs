bad2386b355ab5371d5c578e11dc66f8b2cd3fc1
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-23 09:03:20 +0100

    feature: add auth view model

---
+++ firenotes-api/Models/View/AuthViewModel.cs
@@ -0,0 +1,19 @@
+/**
+ * Created by winner-timothybolorunduro on 23/11/2017.
+ */
+ 
+ namespace firenotes_api.Models.View
+ {
+     public class AuthViewModel
+     {
+         public string Token { get; set; }
+         
+         public string Id { get; set; }
+
+         public string Email { get; set; }
+
+         public string FirstName { get; set; }
+
+         public string LastName { get; set; }
+     }
+ }
