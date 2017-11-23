ecd5a6faad5fd219d8c4524d30ef9a32f02a5e10
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-23 12:57:55 +0100

    feature: add config tests

---
+++ firenotes-api.Tests/Unit/Configuration/Config.Tests.cs
@@ -0,0 +1,17 @@
+﻿using firenotes_api.Configuration;
+using FluentAssertions;
+using Xunit;
+
+namespace firenotes_api.Tests.Unit.Configuration
+{
+    public class Config_Tests
+    {
+        [Fact(Skip = "I have to figure out a way to test this properly")]
+        public void TheConfigPropertiesAreNotEmply()
+        {
+            Config.DbPath.Should().NotBeNullOrWhiteSpace();
+            Config.Secret.Should().NotBeNullOrWhiteSpace();
+            Config.DbPath.Should().Be("mongodb://localhost:27017");
+        }
+    }
+}
