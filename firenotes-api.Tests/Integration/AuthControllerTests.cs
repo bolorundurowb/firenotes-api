fd7762c2f8c86e3812e936dea5bca4ab4f7074bd
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-25 16:03:43 +0100

    feature: add signup tests

--- firenotes-api.Tests/Integration/AuthControllerTests.cs
+++ firenotes-api.Tests/Integration/AuthControllerTests.cs
@@ -74,6 +74,33 @@ namespace firenotes_api.Tests.Integration
             responseString.Should().Be("The password cannot be less than 8 characters.");
         }
 
+        [Fact]
+        public async void SuccessfulSignUp()
+        {
+            StringContent stringContent = new StringContent(
+                "{ \"email\": \"name@email.com\", \"password\": \"12345678\" }",
+                UnicodeEncoding.UTF8,
+                "application/json");
+            var response = await _client.PostAsync("/api/auth/signup", stringContent);
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.Should().Contain("token");
+            responseString.Should().Contain("email");
+        }
+
+        [Fact]
+        public async void ConflictIfAUserWithTheEmailExists()
+        {
+            StringContent stringContent = new StringContent(
+                "{ \"email\": \"name@email.com\", \"password\": \"12345679\" }",
+                UnicodeEncoding.UTF8,
+                "application/json");
+            var response = await _client.PostAsync("/api/auth/signup", stringContent);
+            response.StatusCode.Should().Be(HttpStatusCode.Conflict);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString.Should().Be("Sorry, a user with that email already exists.");
+        }
+
         #endregion
 
         #region Login
