0b3f132eae2207c4e053482372b070f6da0f6366
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-25 15:47:46 +0100

    feature: uodate notes to reference owner

--- firenotes-api/Models/Data/Note.cs
+++ firenotes-api/Models/Data/Note.cs
@@ -14,6 +14,8 @@ namespace firenotes_api.Models.Data
          [BsonId]
          public string Id { get; set; }
 
+         public string Owner { get; set; }
+
          public string Title { get; set; }
 
          public string Details { get; set; }
