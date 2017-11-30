4239eacdf97639f5a725642b40aa996acdef4e35
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-11-30 13:59:45 +0100

    feature: add note retrieval query model

---
+++ firenotes-api/Models/Binding/NoteQueryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace firenotes_api.Models.Binding
+{
+    public class NoteQueryModel
+    {
+        public int Limit { get; set; }
+
+        public string Tag { get; set; }
+
+        public int Skip { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
