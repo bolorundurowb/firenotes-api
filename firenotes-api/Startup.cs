f01355ebb12e9fba9a84a398c2d0417595c3b596
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-12-02 16:16:04 +0100

    chore: remove unused usings

--- firenotes-api/Startup.cs
+++ firenotes-api/Startup.cs
@@ -3,7 +3,6 @@ using dotenv.net;
 using firenotes_api.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
