5ca6387a2fea577538fb1ccb721f754f03f3e63f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-22 12:46:33 +0100

    #9 | complete the handlebars generating method

--- firenotes-api/Configuration/EmailTemplates.cs
+++ firenotes-api/Configuration/EmailTemplates.cs
@@ -1,10 +1,38 @@
-﻿namespace firenotes_api.Configuration
+﻿using System.IO;
+using System.Reflection;
+using HandlebarsDotNet;
+
+namespace firenotes_api.Configuration
 {
     public class EmailTemplates
     {
-        public static string GetForgotPasswordEmail()
+        public static string GetForgotPasswordEmail(object data)
         {
+            string templateName = "ForgotPassword";
+
+            string handlebars;
             
+            using (var stream = GetTemplate(templateName))
+            {
+                if (stream == null)
+                {
+                    handlebars = string.Empty;
+                }
+                using (var streamReader = new StreamReader(stream))
+                {
+                    handlebars = streamReader.ReadToEnd();
+                }
+            }
+
+            var template = Handlebars.Compile(handlebars);
+            return template(data);
+        }
+
+        private static Stream GetTemplate(string templateName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var fileNamespace = "firenotes_api.Configuration.EmailTemplates" + templateName + ".hbs";
+            return assembly.GetManifestResourceStream(fileNamespace);
         }
     }
 }
