d9370788c17ad96b5047fd341c910f4c631fe94a
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-22 15:43:27 +0100

    #25 | update the gitignore

--- firenotes-api/Startup.cs
+++ firenotes-api/Startup.cs
@@ -23,7 +23,7 @@ namespace firenotes_api
         }
 
         public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
