b23917918b927f3c7de4b7289558d3582900951d
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-22 23:05:14 +0100

    #9 | complete mandrill email sending method

--- firenotes-api/Configuration/Email.cs
+++ firenotes-api/Configuration/Email.cs
@@ -1,10 +1,23 @@
-﻿namespace firenotes_api.Configuration
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mandrill;
+using Mandrill.Model;
+
+namespace firenotes_api.Configuration
 {
-    public class Email
+    public static class Email
     {
-        public static void Send(object payload)
+        private static MandrillApi Api;
+        
+        static Email()
         {
-            
+            Api = new MandrillApi(Config.MandrillApiKey);
+        }
+        
+        public static async Task<IList<MandrillSendMessageResponse>> Send(string recipient, string subject, string payload)
+        {
+            var message = new MandrillMessage(Config.ServiceEmail, recipient, subject, payload);
+            return await Api.Messages.SendAsync(message);
         }
     }
 }
