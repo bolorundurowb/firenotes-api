1daa310ab9c06996ef6c3864e6919dc4364dd9b6
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-25 15:38:05 +0100

    #11 | add the users controller scaffold

---
+++ firenotes-api/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firenotes_api.Controllers
+{
+    public class UsersController : Controller
+    {
+        [Route("{id}"), HttpDelete]
+        public async Task<IActionResult> ArchiveUser()
+        {
+            return Ok();
+        }
+    }
+}
