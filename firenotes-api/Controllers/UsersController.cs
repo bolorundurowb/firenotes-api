a2ddb1197b4c595dec31f7fc3292d929765f6345
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-25 15:58:59 +0100

    #11 | complete the archiving controller method

--- firenotes-api/Controllers/UsersController.cs
+++ firenotes-api/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using MongoDB.Driver;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using firenotes_api.Configuration;
+using firenotes_api.Models.Data;
 
 namespace firenotes_api.Controllers
 {
@@ -21,9 +22,51 @@ namespace firenotes_api.Controllers
             _mongoDatabase = mongoClient.GetDatabase(Startup.DatabaseName);
         }
         
-        [Route("{id}"), HttpDelete]
-        public async Task<IActionResult> ArchiveUser()
+        // POST api/users/:id/archive
+        [Route("{id}/archive"), HttpPost]
+        public async Task<IActionResult> ArchiveUser(string id)
         {
+            var callerId = HttpContext.Items["id"].ToString();
+
+            if (callerId != id)
+            {
+                return BadRequest("You can only archive your own account.");
+            }
+            
+            var usersCollection = _mongoDatabase.GetCollection<User>("users");
+            
+            var filterBuilder = Builders<User>.Filter;
+            var filter = filterBuilder.Eq("_id", id);
+            
+            var updateBuilder = Builders<User>.Update;
+            var update = updateBuilder.Set("IsArchived", true);
+            
+            await usersCollection.UpdateOneAsync(filter, update);
+            
+            return Ok();
+        }
+        
+        // PUT api/users/:id
+        [Route("{id}"), HttpPut]
+        public async Task<IActionResult> Update(string id)
+        {
+            var callerId = HttpContext.Items["id"].ToString();
+
+            if (callerId != id)
+            {
+                return BadRequest("You can only update your own profile.");
+            }
+            
+            var usersCollection = _mongoDatabase.GetCollection<User>("users");
+            
+            var filterBuilder = Builders<User>.Filter;
+            var filter = filterBuilder.Eq("_id", id);
+            
+            var updateBuilder = Builders<User>.Update;
+            var update = updateBuilder.Set("IsArchived", true);
+            
+            await usersCollection.UpdateOneAsync(filter, update);
+            
             return Ok();
         }
     }
