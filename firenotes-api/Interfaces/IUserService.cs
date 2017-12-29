470fd493434236a4882641cf0f016253b97c9899
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-29 17:55:42 +0100

    #36 | add the user service interface

---
+++ firenotes-api/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using firenotes_api.Models.Data;
+
+namespace firenotes_api.Interfaces
+{
+    public interface IUserService
+    {
+        User GetUser(string id);
+
+        void Update(string id, User user);
+
+        void Archive(string id);
+    }
+}
