895cc1c0310dc6f35c669dc85b773b563fb87e2f
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-30 16:16:34 +0100

    #29 | set test env vars

--- firenotes-api.Tests/Integration/BaseApiControllerTests.cs
+++ firenotes-api.Tests/Integration/BaseApiControllerTests.cs
@@ -21,7 +21,10 @@ namespace firenotes_api.Tests.Integration
             Environment.SetEnvironmentVariable("MONGO_URL", "mongodb://localhost:27017/");
             Environment.SetEnvironmentVariable("SECRET", "test-secret");
             Environment.SetEnvironmentVariable("SERVICE_EMAIL", "test@email.org");
-            Environment.SetEnvironmentVariable("MANDRILL_API_KEY", "UrZSSLzdVKwrTgOlVADV_w");
+            Environment.SetEnvironmentVariable("MAILGUN_API_KEY", "UrZSSLzdVKwrTgOlVADV_w");
+            Environment.SetEnvironmentVariable("MAILGUN_BASE_URI", "https://api.mailgun.net/v3/");
+            Environment.SetEnvironmentVariable("MAILGUN_REQUEST_URI", "sandbox6413747fa8049658100b1b2b327f99e.mailgun.org");
+            Environment.SetEnvironmentVariable("MAILGUN_SMTP_LOGIN", "postmaster@sandbox6413747fa8049658100b1b2b327f99e.mailgun.org");
         }
     }
 }
