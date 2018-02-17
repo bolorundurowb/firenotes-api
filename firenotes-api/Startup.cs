a28de57fcf032752753cab16eec21df378183d69
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2018-02-18 00:23:01 +0100

    #46 | mild fix

--- firenotes-api/Startup.cs
+++ firenotes-api/Startup.cs
@@ -22,7 +22,7 @@ namespace firenotes_api
             _environment = env;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
         private IHostingEnvironment _environment;
         
         // This method gets called by the runtime. Use this method to add services to the container.
